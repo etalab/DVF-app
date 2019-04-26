a58a580c015ca1ddef3d27f09342653241efbc0a
Aurelien Cousin <acousin@leadformance.com>
2019-04-26 14:11:15 +0200

    Remove FAQ

--- app.py
+++ app.py
@@ -25,12 +25,6 @@ def dates():
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
-
-
-@app.route('/faq.html')
-def rootFAQ():
-	return app.send_static_file('faq.html')
-
 	
 @app.route('/css/<path:path>')
 def send_css(path):
