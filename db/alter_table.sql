fa254f07a52b8de351777cc151f9df0f7d9d0963
Marion P <marion.paclot@gmail.com>
2019-05-07 15:41:24 +0200

    Patch renommage nature de terrain
    Erreur sur les tables de nature de culture. Ces deux lignes renomment les terrains AB et ABREUV (1ère ligne de chaque fichier) en attendant le réimport de la nouvelle base

--- db/alter_table.sql
+++ db/alter_table.sql
@@ -12,3 +12,7 @@ CREATE INDEX idx_date ON dvf(date_mutation) ;
 CREATE INDEX idx_parcelle ON dvf(id_parcelle) ;
 CREATE INDEX idx_section_commune ON dvf(code_commune, section_prefixe) ;
 CREATE INDEX id_mutation ON dvf(id_mutation) ;
+
+-- Patch en attendant la correction dans la base initiale
+UPDATE dvf SET nature_culture = 'Terrain à bâtir' WHERE code_nature_culture = 'AB';
+UPDATE dvf SET nature_culture_speciale = 'Abreuvoirs' WHERE code_nature_culture_speciale = 'ABREU';
