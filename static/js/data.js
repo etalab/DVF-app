fcbc96ade100fa82746247192230d07c5980bcef
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-05-09 23:38:24 +0200

    Move data functions to a separate file

---
+++ static/js/data.js
@@ -0,0 +1,93 @@
+function getRemoteJSON(url, throwIfNotFound) {
+	return fetch(url).then(function (response) {
+		if (response.ok) {
+			return response.json()
+		}
+
+		if (response.status === 404 && !throwIfNotFound) {
+			return
+		}
+
+		throw new Error('Impossible de récupérer les données demandées : ' + response.status)
+	})
+}
+
+var communesMappingPromise = getRemoteJSON('/donneesgeo/communes-mapping.json', true)
+
+function getCadastreLayer(layerName, codeCommune) {
+	return communesMappingPromise.then(function (communesMapping) {
+		const communesToGet = codeCommune in communesMapping ? communesMapping[codeCommune] : [codeCommune]
+		return Promise.all(communesToGet.map(function (communeToGet) {
+			return getRemoteJSON(`https://cadastre.data.gouv.fr/bundler/cadastre-etalab/communes/${communeToGet}/geojson/${layerName}`)
+		})).then(function (featureCollections) {
+			return {
+				type: 'FeatureCollection',
+				features: featureCollections.reduce(function (acc, featureCollection) {
+					if (featureCollection && featureCollection.features) {
+						return acc.concat(featureCollection.features)
+					}
+
+					return acc
+				}, [])
+			}
+		})
+	})
+}
+
+function getParcelles(codeCommune) {
+	return getCadastreLayer('parcelles', codeCommune)
+}
+
+function getSections(codeCommune) {
+	return getCadastreLayer('sections', codeCommune)
+}
+
+function computeParcelle(mutationsSection, idParcelle) {
+	var mutationsParcelle = mutationsSection.filter(function (m) {
+		return m.id_parcelle === idParcelle
+	})
+
+	var mutations = _.chain(mutationsParcelle)
+		.groupBy('id_mutation')
+		.map(function (rows, idMutation) {
+			var infos = [_.pick(rows[0], 'date_mutation', 'id_parcelle', 'nature_mutation', 'valeur_fonciere')]
+
+			var parcellesLiees = _.uniq(
+				mutationsSection
+					.filter(function (m) {
+						return m.id_mutation === idMutation && m.id_parcelle !== idParcelle
+					})
+					.map(function (m) {
+						return m.id_parcelle
+					})
+			)
+
+			var batiments = _.chain(rows)
+				.filter(function (m) {
+					return m.type_local !== 'None'
+				})
+				.uniqBy(function (m) {
+					return `${m.code_type_local}@${m.surface_reelle_bati}`
+				})
+				.value()
+
+			var terrains = _.chain(rows)
+				.filter(function (m) {
+					return m.nature_culture !== 'None'
+				})
+				.uniqBy(function (m) {
+					return `${m.code_nature_culture}@${m.code_nature_culture_special}@${m.surface_terrain}`
+				})
+				.value()
+
+			return {
+				infos: infos,
+				parcellesLiees: parcellesLiees,
+				batiments: batiments,
+				terrains: terrains
+			}
+		})
+		.value()
+
+	return { mutations: mutations }
+}
