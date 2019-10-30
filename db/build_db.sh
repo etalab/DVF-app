a1a3be36c85804e676cdd8d1a4b94f9f915b4cb8
Marion Paclot <marion.paclot@gmail.com>
2019-10-30 15:35:13 +0100

    Mise à jour d'octobre 2019

--- db/build_db.sh
+++ db/build_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script de creation de la base de donnees PostgreSQL 
+# Script de création de la base de donnees PostgreSQL 
 DIR=$(echo $(dirname $0))
 cd $DIR
 
@@ -8,20 +8,20 @@ sudo -u postgres psql -c "CREATE DATABASE dvf2;"
 sudo -u postgres psql -c "ALTER DATABASE dvf2 SET datestyle TO ""ISO, DMY"";"
 sudo -u postgres psql -d dvf2 -f "create_table.sql"
 
-# Chargement des données 
+# Chargement des données sur le serveur
 DATADIR="data"
 mkdir -p $DATADIR
 
-for YEAR in 2014 2015 2016 2017 2018
+for YEAR in 2014 2015 2016 2017 2018 2019
 do
-  [ ! -f $DATADIR/full_$YEAR.csv.gz ] && wget -r -np -nH --cut-dirs 5  https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/$YEAR/full.csv.gz -O $DATADIR/full_$YEAR.csv.gz
+  [ ! -f $DATADIR/full_$YEAR.csv.gz ] && wget -r -np -nH -N --cut-dirs 5  https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/$YEAR/full.csv.gz -O $DATADIR/full_$YEAR.csv.gz
 done
 
 find $DATADIR -name '*.gz' -exec gunzip -f '{}' \;
 
-#Chargement des données
+#Chargement des données dans postgres
 DATAPATH=$( cd $DATADIR ; pwd -P )
-for YEAR in 2014 2015 2016 2017 2018
+for YEAR in 2014 2015 2016 2017 2018 2019
 do
   sudo -u postgres psql -d dvf2 -c "COPY dvf FROM '$DATAPATH/full_$YEAR.csv' delimiter ',' csv header encoding 'UTF8';"
 done
