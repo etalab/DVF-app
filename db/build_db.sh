1c75fb250cbba51f95b848bf2c527e2925200479
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-08 14:59:07 +0100

    Millésime octobre 2020

--- db/build_db.sh
+++ db/build_db.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Script de création de la base de donnees PostgreSQL 
+# Script de création de la base de donnees PostgreSQL
 DIR=$(echo $(dirname $0))
 cd $DIR
 
-sudo -u postgres psql -c "DROP DATABASE IF EXISTS dvf_202004;"
-sudo -u postgres psql -c "CREATE DATABASE dvf_202004;"
-sudo -u postgres psql -c "ALTER DATABASE dvf_202004 SET datestyle TO ""ISO, DMY"";"
-sudo -u postgres psql -d dvf_202004 -f "create_table.sql"
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS dvf_202010;"
+sudo -u postgres psql -c "CREATE DATABASE dvf_202010;"
+sudo -u postgres psql -c "ALTER DATABASE dvf_202010 SET datestyle TO ""ISO, DMY"";"
+sudo -u postgres psql -d dvf_202010 -f "create_table.sql"
 
 # Chargement des données sur le serveur
 DATADIR="data"
 mkdir -p $DATADIR
 
-for YEAR in 2014 2015 2016 2017 2018 2019
+for YEAR in 2014 2015 2016 2017 2018 2019 2020
 do
   [ ! -f $DATADIR/full_$YEAR.csv.gz ] && wget -r -np -nH -N --cut-dirs 5  https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/$YEAR/full.csv.gz -O $DATADIR/full_$YEAR.csv.gz
 done
@@ -21,9 +21,9 @@ find $DATADIR -name '*.gz' -exec gunzip -f '{}' \;
 
 #Chargement des données dans postgres
 DATAPATH=$( cd $DATADIR ; pwd -P )
-for YEAR in 2014 2015 2016 2017 2018 2019
+for YEAR in 2014 2015 2016 2017 2018 2019 2020
 do
-  sudo -u postgres psql -d dvf_202004 -c "COPY dvf FROM '$DATAPATH/full_$YEAR.csv' delimiter ',' csv header encoding 'UTF8';"
+  sudo -u postgres psql -d dvf_202010 -c "COPY dvf FROM '$DATAPATH/full_$YEAR.csv' delimiter ',' csv header encoding 'UTF8';"
 done
 
 # Ajout d'une colonne et d'index - Assez long
--- static/js/index.js
+++ static/js/index.js
@@ -24,8 +24,8 @@ vue.$watch('fold_left', function () {
 
 // Définition des variables globales
 
-var MIN_DATE = '2015-01-01'
-var MAX_DATE = '2019-12-31'
+var MIN_DATE = '2014-01-01'
+var MAX_DATE = '2020-06-31'
 
 var map = null;
 var mapLoaded = false;
